febf846bc11a987621f20266fe303a82f759e1e9
Anonymous <anonymous>
2025-12-06 14:50:54 +0100

    new layout and nav-item

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'SI9INT | Intelligenjiza';
+export const SITE_TITLE = 'Intelligenjiza';
 export const SITE_DESCRIPTION = 'A collection of information related to (offensive) cyber- and IT-security';
